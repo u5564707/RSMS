aa26e84848eae107567c185744801c7bcee0d992
Tom Kocik <u5564707@uds.anu.edu.au>
2016-06-01 02:34:40 +1000

    cleaned up code, refined tree definition

--- databaseFunctions.js
+++ databaseFunctions.js
@@ -7,8 +7,8 @@
 var Datastore = require('nedb')
   , db
 
-function createDummyDatastore() {
-    db = new Datastore({ filename: 'datastore', autoload: true });
+function createTestData1() {
+    db = new Datastore({ filename: 'testData1', autoload: true });
 
     // insert processes
     db.insert({ _id: "rootProcess", attributeNames: ["mass (mg)"] });
@@ -19,29 +19,43 @@ function createDummyDatastore() {
     db.insert({ _id: "node2", parentID: "rootNode", processName: "DNA Extract" });
 
     // insert samples
-    db.insert({ _id: { nodeID: "rootNode", sampleID: "1" }, attributes: ["50"] });
-    db.insert({ _id: { nodeID: "rootNode", sampleID: "2" }, attributes: ["55"] });
-    db.insert({ _id: { nodeID: "rootNode", sampleID: "3" }, attributes: ["80"] });
-    db.insert({ _id: { nodeID: "rootNode", sampleID: "4" }, attributes: ["60"] });
-    db.insert({ _id: { nodeID: "node2", sampleID: "1" }, attributes: ["12/3", "Qiagen", "160", "12", ""] });
-    db.insert({ _id: { nodeID: "node2", sampleID: "2" }, attributes: ["12/3", "Qiagen", "160", "18", ""] });
-    db.insert({ _id: { nodeID: "node2", sampleID: "3" }, attributes: ["12/3", "Qiagen", "80", "10", "Second elution failed, so only 80mL"] });
+    db.insert({ _id: { nodeID: "rootNode", sampleID: "1" }, attributeData: ["50"] });
+    db.insert({ _id: { nodeID: "rootNode", sampleID: "2" }, attributeData: ["55"] });
+    db.insert({ _id: { nodeID: "rootNode", sampleID: "3" }, attributeData: ["80"] });
+    db.insert({ _id: { nodeID: "rootNode", sampleID: "4" }, attributeData: ["60"] });
+    db.insert({ _id: { nodeID: "node2", sampleID: "1" }, attributeData: ["12/3", "Qiagen", "160", "12", ""] });
+    db.insert({ _id: { nodeID: "node2", sampleID: "2" }, attributeData: ["12/3", "Qiagen", "160", "18", ""] });
+    db.insert({ _id: { nodeID: "node2", sampleID: "3" }, attributeData: ["12/3", "Qiagen", "80", "10", "Second elution failed, so only 80mL"] });
 }
 
-// return all attribute names from the process of nodeID as an array
-function getProcessAttributeNames(nodeID, callback) {
-    db.findOne({ _id: nodeID }, function(err, node) {
-        db.findOne({ _id: node.processName }, function(err, process) {
-            callback(process.attributeNames);
-        });
-    });
-}
+function createTestData2() {
+    db = new Datastore({ filename: 'testData2', autoload: true });
 
-// return all nodes as an array of documents
-function getAllNodes(callback) {
-    db.find({ parentID: { $exists: true } }, function(err, nodes) {
-        callback(nodes);
-    });
+    // insert processes
+    db.insert({ _id: "rootProcess", attributeNames: ["mass (mg)"] });
+    db.insert({ _id: "DNA Extract", attributeNames: ["Date Extracted", "Kit", "Elution mL", "DNA ng", "Notes"] });
+
+    // insert nodes
+    db.insert({ _id: "rootNode", parentID: null, processName: "rootProcess" });
+    db.insert({ _id: "node2", parentID: "rootNode", processName: "DNA Extract" });
+    db.insert({ _id: "node3", parentID: "rootNode", processName: "DNA Extract" });
+    db.insert({ _id: "node4", parentID: "node3", processName: "DNA Extract" });
+
+    // insert samples
+    db.insert({ _id: { nodeID: "rootNode", sampleID: "1" }, attributeData: ["50"] });
+    db.insert({ _id: { nodeID: "rootNode", sampleID: "2" }, attributeData: ["55"] });
+    db.insert({ _id: { nodeID: "rootNode", sampleID: "3" }, attributeData: ["80"] });
+    db.insert({ _id: { nodeID: "rootNode", sampleID: "4" }, attributeData: ["60"] });
+    db.insert({ _id: { nodeID: "node2", sampleID: "1" }, attributeData: ["12/3", "Qiagen", "160", "12", ""] });
+    db.insert({ _id: { nodeID: "node2", sampleID: "2" }, attributeData: ["12/3", "Qiagen", "160", "18", ""] });
+    db.insert({ _id: { nodeID: "node2", sampleID: "3" }, attributeData: ["12/3", "Qiagen", "80", "10", "Second elution failed, so only 80mL"] });
+    db.insert({ _id: { nodeID: "node3", sampleID: "1" }, attributeData: ["50"] });
+    db.insert({ _id: { nodeID: "node3", sampleID: "2" }, attributeData: ["55"] });
+    db.insert({ _id: { nodeID: "node3", sampleID: "3" }, attributeData: ["80"] });
+    db.insert({ _id: { nodeID: "node3", sampleID: "4" }, attributeData: ["60"] });
+    db.insert({ _id: { nodeID: "node4", sampleID: "1" }, attributeData: ["12/3", "Qiagen", "160", "12", ""] });
+    db.insert({ _id: { nodeID: "node4", sampleID: "2" }, attributeData: ["12/3", "Qiagen", "160", "18", ""] });
+    db.insert({ _id: { nodeID: "node4", sampleID: "3" }, attributeData: ["12/3", "Qiagen", "80", "10", "Second elution failed, so only 80mL"] });
 }
 
 // return the node with nodeID as a document
@@ -52,9 +66,18 @@ function getNode(nodeID, callback) {
 }
 
 // return all child nodes of nodeID as an array of documents
-function getChildNodes(nodeID, callback) {
-    db.find({ parentID: nodeID }, function(err, nodes) {
+function getNonRootNodes(nodeID, callback) {
+    db.find({ parentID: { $exists: true }, _id: { $ne: "rootNode" } }, function(err, nodes) {
         callback(nodes);
+    });
+}
+
+// return all attribute names from the process of nodeID as an array
+function getProcessAttributeNames(nodeID, callback) {
+    db.findOne({ _id: nodeID }, function(err, node) {
+        db.findOne({ _id: node.processName }, function(err, process) {
+            callback(process.attributeNames);
+        });
     });
 }
 
